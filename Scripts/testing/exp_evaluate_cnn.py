659de4a8d919516acaf0d6bef4fa37d94b142b4c
Akira Sewnath <akira.sewnath@gmail.com>
2018-07-23 14:47:23 -0400

    7_23_18 work

--- Scripts/testing/exp_evaluate_cnn.py
+++ Scripts/testing/exp_evaluate_cnn.py
@@ -9,32 +9,85 @@ from cnn_functions import graph_pred_truth
 from exp_functions import exp_create_input
 from cnn_functions import retrieve_grid
 from exp_functions import border_grid
+from exp_functions import pred_exp_create_input
+from exp_functions import get_detrend_grid
 
 #MAKE SURE THAT THESE MATCH THE PARAMETERS IN TRAINING
-imageSize = 5
+imageSize = 7
 numForecast = 2
 resolution = 100
 
 regBool = 1
 
 #Load model for evaluation
-model = keras.models.load_model('model72018_1month_comp2.h5')
+model = keras.models.load_model('model72318_2.h5')
 
 #Get test data to retrieve predictions for month+1
 year  = 2015
-month = 8 #June
+month = 7 #June
+fMonth = month+1
 
 data, _, size = exp_create_input(month, year, numForecast, imageSize, resolution)
 data   = np.float32(data)
 
-month_1_pred = model.predict(data)
+outputMonth1 = model.predict(data)
+
 #Get sea ice concentration
-month_1_pred = month_1_pred[:,0]
+predMonth1 = outputMonth1[:,0]
 dim = int(np.sqrt(size))
-month_1_pred = np.reshape(month_1_pred, (dim, dim))
-#border with zeroes to see it
-month_1_pred = border_grid(month_1_pred, 2)
-graph_pred_truth(month_1_pred, month+1, year, resolution) #July
-#grid = retrieve_grid(month+1, year, resolution)
+predMonth1 = np.reshape(predMonth1, (dim, dim))
+graph_pred_truth(predMonth1, fMonth, year, resolution) #July
+
+startYear = 1985
+stopYear = 2014
+detrendGrid = get_detrend_grid(month, startYear, stopYear, resolution)
+graph_pred_truth(detrendGrid, fMonth, year, resolution) #July
+
+
+groundTruth = retrieve_grid(fMonth, year, resolution)
+
+#Root mean square error of the ice areas (assuming each grid cell is 100 km)
+#cnnError = np.sum(np.absolute(groundTruth-predMonth1)) / (57**2)
+#linError = np.sum(np.absolute(groundTruth-detrendGrid)) / (57**2)
+
+
+cnnArea = predMonth1 * 100
+linArea = detrendGrid * 100
+gtArea  = groundTruth * 100
+
+rmseCnn = np.sqrt((np.sum(cnnArea-gtArea)**2)/(57**2))
+rmseLin = np.sqrt((np.sum(linArea-gtArea)**2)/(57**2))
+
+
+#Calculate ice extents
+#cnnNoIce = np.where(predMonth1 >=0.15)
+#cnnIceExtent = predMonth
+
+
+'''
+iceConc  = outputMonth1[:,0]
+iceConc = np.reshape(iceConc, (dim, dim))
+iceThick = outputMonth1[:,1]
+iceThick = np.reshape(iceThick, (dim, dim))
+
+procMonth1 = np.stack((iceConc, iceThick))
+'''
+'''
+#Get test data to retrieve predictions for month+2
+month2 = month+1
+monthList = [procMonth1]
+data, _, _ = pred_exp_create_input(monthList, month2, year, numForecast, imageSize, resolution)
+data = np.float32(data)
+
+outputMonth2 = model.predict(data)
+predMonth2 = outputMonth2[:,0]
+predMonth2 = np.reshape(predMonth2, (dim, dim))
+graph_pred_truth(predMonth2, month2+1, year, resolution) #August
+'''
+
+
+
+
+
 
 
--- Scripts/testing/exp_functions.py
+++ Scripts/testing/exp_functions.py
@@ -8,12 +8,48 @@ from mpl_toolkits.basemap import Basemap
 from scipy.interpolate import griddata
 from netCDF4 import Dataset
 import forecast_funcs as ff
+import statsmodels.api as sm
+from scipy import stats
 
 from cnn_functions import get_lat_lon_arr
 from cnn_functions import retrieve_grid
 from cnn_functions import get_ice_thickness
 from cnn_functions import get_month_str
 #import numpy as np
+
+def get_detrend_grid(month, startYear, stopYear, resolution):
+
+    #get all sea ice concentration grids for the specific month and stack them
+    years = np.arange(startYear, stopYear+1)
+
+    monthIceConc = retrieve_grid(month, startYear, resolution)
+    for year in range(startYear+1, stopYear+1):
+        monthIceConc = np.dstack((monthIceConc, retrieve_grid(month, year, resolution)))
+
+    forMonthIceConc = retrieve_grid(month, stopYear+1, 100)
+    #for each cell vector, do all that detrending stuff, make list
+    finalCellForecast = []
+    for row in range(np.size(monthIceConc, 0)):
+        for col in range(np.size(monthIceConc, 1)):
+            vect = monthIceConc[row, col, :]
+            
+            trendT, interceptT, _, _, _ = stats.linregress(years,vect)
+            forecast = trendT*(stopYear+1) + interceptT
+            #lineT = (trendT*years) + interceptT
+            #vectDT=vect-lineT
+            #model=sm.OLS(vectDT, vect)
+            #fit=model.fit()
+            #cellForecast = fit.predict(forMonthIceConc[row, col])[0]
+            
+            #cellTrendP =(lineT[-1]+(lineT[-1]-lineT[-2]))
+            #finalCellForecast.append(cellForecast + cellTrendP)
+            finalCellForecast.append(forecast)
+    
+    #reshape list into final grid
+    finalCellForecast = np.reshape(finalCellForecast, (57, 57))        
+    return finalCellForecast
+    
+
 
 def border_grid(grid, padding):
     
@@ -28,8 +64,95 @@ def border_grid(grid, padding):
     
     return grid
 
-grid = retrieve_grid(6, 2013, 100)
+def pred_exp_create_input(monthList, month, year, numForecast, imDim, resolution):
 
+    mat = []
+    matYear = year
+    monthMod = month
+    subVal = 0
+    padding = int(np.floor(imDim/2))
+    
+    #Pad the predictions:
+    for index in range(np.size(monthList, 0)):
+        grid = monthList[index]
+        iceConcGrid = grid[0]
+        iceConcGrid = border_grid(iceConcGrid, padding)
+        mat.append(iceConcGrid)
+        iceThickGrid = grid[1]
+        iceThickGrid = border_grid(iceThickGrid, padding)
+        mat.append(iceThickGrid)
+        if(monthMod-1 < 0):
+            monthMod = 11
+        else:    
+            monthMod = monthMod - 1
+        #if(index > 0):    
+        numForecast = numForecast - 1
+   
+       
+    for index in range(numForecast+1):
+        
+        if(monthMod-subVal < 0):
+            monthMod = 11
+            matYear = matYear - 1
+            grid = retrieve_grid(monthMod, matYear, resolution)
+            grid = border_grid(grid, padding)
+            subVal = 0 #reset value to subtract 
+            mat.append(grid)
+            iceThickness = get_ice_thickness(monthMod, matYear, resolution)
+            iceThickness = border_grid(iceThickness, padding)
+            mat.append(iceThickness/100) #scaling  
+        else:    
+            grid = retrieve_grid(monthMod-subVal, matYear, resolution)
+            grid = border_grid(grid, padding)
+            mat.append(grid)
+            iceThickness = get_ice_thickness(monthMod-subVal, matYear, resolution)
+            iceThickness = border_grid(iceThickness, padding)
+            mat.append(iceThickness/100) #scaling    
+        
+        subVal = subVal+1
+    
+    xDim = np.size(mat[0], 0)
+    
+    #Create pixel coordinate matricies (includes the borders)
+    xVect = np.arange(0, xDim) / xDim
+    xMat = np.tile(xVect, (xDim, 1))
+    yMat = xMat.transpose()
+    mat.append(xMat)
+    mat.append(yMat)
+    
+    mat = np.reshape(mat, (np.size(mat,0),np.size(mat[0],0),np.size(mat[0],0)))
+     
+    gtMat = []
+    #Get ground truth data.. (account for January transition)
+    if(month == 11):
+        gtMat.append(retrieve_grid(1, year+1, resolution))
+        iceThickness = get_ice_thickness(0, year+1, resolution)
+        gtMat.append(iceThickness/100)
+    else:   
+        gtMat.append(retrieve_grid(month+1, year, resolution))
+        iceThickness = get_ice_thickness(month+1, year, resolution)
+        gtMat.append(iceThickness/100)    
+    gtMat = np.reshape(gtMat, (np.size(gtMat,0),np.size(gtMat[0],0),np.size(gtMat[0],0)))
+    
+    #Retrieve grid dimensions
+    matChannels = np.size(mat, 0)
+    matRows = np.size(mat[0],0)
+    matCols = np.size(mat[0],1)
+    gtChannels = np.size(gtMat, 0)
+    
+    inputs = []
+    gt = []
+    #Create sliding window to extract volumes and add them to list
+    for row in range(matRows-(2*padding)):
+        for col in range(matCols-(2*padding)):
+            inputs.append(mat[0:matChannels, row:row+imDim, col:col+imDim])
+            gt.append(gtMat[0:gtChannels, row, col])
+        
+    size = np.size(inputs,0) 
+
+    return inputs, gt, size
+    
+    
 def exp_create_input(month, year, numForecast, imDim, resolution):
 
     mat = []
